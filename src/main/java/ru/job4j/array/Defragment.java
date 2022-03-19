cab5d11b23d4f0d739a304528b751f0db101c1a1
ValeraPi <valerqa@gmail.com>
2022-03-19 17:44:56 +0300

    6.8. Дефрагментация массива [#225 #277207]

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -6,10 +6,8 @@ public class Defragment {
             if (array[index] == null) {
                 for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
-                        String temp = "";
-                        temp = array[index];
                         array[index] = array[i];
-                        array[i] = temp;
+                        array[i] = null;
                         break;
                     }
                 }
