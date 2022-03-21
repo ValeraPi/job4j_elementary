b7a92cdb349a284aaaf9beb18d06cd4d210c15b6
ValeraPi <valerqa@gmail.com>
2022-03-21 12:34:41 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #277357]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -11,7 +11,7 @@ public class Machine {
                     while (change >= coin) {
                         change -= coin;
                         rsl[size] = coin;
-                        size += 1;
+                        size++;
                     }
                 }
         return Arrays.copyOf(rsl, size);
